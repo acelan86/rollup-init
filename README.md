0b363142e0efc4024846de6f08b90683dadcd72b
acelan86 <79480491@qq.com>
2017-05-03 18:12:40 +0800

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue-ga
+Vue tool for generating ads framework
+
+### 快速开始
+`npm i -g https://github.com/galaa2011/vue-ga.git`
+
+#### 基础格式
+`ga create <type> [path] [-f]`
+
+- type: 页面类型，可选form 或 list
+- path: 生成路径，默认当前文件夹
+- -f: 强制覆盖
+
+> 例: `ga create form src/pages/advert/create.vue -f`
---
+++ tpl/rollup.config.js
@@ -0,0 +1,45 @@
+import babel from 'rollup-plugin-babel'
+import eslint from 'rollup-plugin-eslint'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import filesize from 'rollup-plugin-filesize'
+import progress from 'rollup-plugin-progress'
+// import visualizer from 'rollup-plugin-visualizer'
+// import livereload from 'rollup-plugin-livereload'
+
+export default {
+  entry: 'src/main.js',
+  dest: 'dist/main.min.js',
+  format: 'iife',
+  sourceMap: true,
+  plugins: [
+    eslint({
+      exclude: [
+        'node_modules/**'
+      ]
+    }),
+    babel({
+      exclude: [
+        'node_modules/**'
+      ]
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    // (process.env.NODE_ENV !== 'production' && livereload({
+    //   watch: 'build'
+    // })),
+    (process.env.NODE_ENV === 'production' && uglify({
+      mangleProperties: {
+        regex: /^_/
+      }
+    })),
+    filesize(),
+    // visualizer({
+    //   filename: 'build/visualizer.html'
+    // }),
+    progress({
+      clearLine: false // default: true
+    })
+  ]
+}
